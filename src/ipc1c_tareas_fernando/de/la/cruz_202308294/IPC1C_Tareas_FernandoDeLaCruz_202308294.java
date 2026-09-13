e0a748d74c0ff6ae5756f0deca605f415dad0496
NandoElias12 <oreonando@gmail.com>
2026-09-13 17:29:38 -0600

    primer commit

---
+++ src/ipc1c_tareas_fernando/de/la/cruz_202308294/IPC1C_Tareas_FernandoDeLaCruz_202308294.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ipc1c_tareas_fernando.de.la.cruz_202308294;
+
+/**
+ *
+ * @author nando_elias12
+ */
+public class IPC1C_Tareas_FernandoDeLaCruz_202308294 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
